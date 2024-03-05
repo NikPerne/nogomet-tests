5232806593e9c1c7965b7eb38dcf88494b843cf6
NikPerne <nik.perne@gmail.com>
2024-03-05 15:25:51 +0100

    Dodal example test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test.beforeAll('login', async ({ page }) => {
+  await page.goto('https://nogomet.onrender.com/');
+  await page.goto('https://nogomet.onrender.com/login');
+  await page.getByPlaceholder('Enter e-mail address').click();
+  await page.getByPlaceholder('Enter e-mail address').fill('nik.perne@gmail.com');
+  await page.getByPlaceholder('Enter password').click();
+  await page.getByPlaceholder('Enter password').fill('Oklop123');
+  await page.getByRole('button', { name: ' Login' }).click();
+  await page.locator('a').filter({ hasText: 'Add Event' }).click({
+    button: 'right'
+  });
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test.afterAll('logout', async ({ page }) => {
+  await page.getByRole('button', { name: ' Nik Perne' }).click();
+  await page.getByText('Logout').click();
+  await page.getByRole('heading', { name: 'Sign in' }).click({button: 'right'});
 });
